f84585ef26f98746ad9df32ff4feb54adf5c8f8d
kevins3204 <kevins3204@kaist.ac.kr>
2019-11-19 16:24:51 +0900

    setup shell modified

--- SparklingFD/setup.sh
+++ SparklingFD/setup.sh
@@ -1,6 +1,13 @@
-install -g yarn 
+npm install -g yarn 
 npm install -g @vue/cli 
+npm install vue-router --save
+npm install axios --save
 vue create frontend
 
 npm install -g express-generator
 express --view=pug backend
+
+cd backend
+npm install
+cd ../frontend
+npm run build
