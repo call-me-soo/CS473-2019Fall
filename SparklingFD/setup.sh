9bf73982d1986b11a21c7b07874bc748ba7744cc
kevins3204 <kevins3204@kaist.ac.kr>
2019-11-24 22:20:18 +0900

    git ignore

--- SparklingFD/setup.sh
+++ SparklingFD/setup.sh
@@ -1,6 +1,26 @@
-install -g yarn 
+npm install -g yarn 
 npm install -g @vue/cli 
+npm install -g vue-router --save
+npm install -g vue-chartjs --save
+npm install -g axios --save
+npm install -g eslint
 vue create frontend
+cd frontend
+vue add vuetify
+npm install chart.js
+npm install jquery
+npm install vue-chartjs
+npm install vue-router
+npm install vue-slick
+npm install vue-slider-component
+cd ../
+
+
 
 npm install -g express-generator
 express --view=pug backend
+
+cd backend
+npm install
+cd ../frontend
+npm run build
