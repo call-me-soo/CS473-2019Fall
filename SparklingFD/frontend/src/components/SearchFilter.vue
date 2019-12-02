b29f870e27bbb3af9d96074ca9c823c6f8588c5f
Suniel <sunnynah05@gmail.com>
2019-12-02 10:00:51 +0900

    implement salary filter modal and indicator text

--- SparklingFD/frontend/src/components/SearchFilter.vue
+++ SparklingFD/frontend/src/components/SearchFilter.vue
@@ -1,7 +1,7 @@
 <template>
 
   <div class="d-inline">
-    <v-flex class="d-inline" v-if="label === '급여'">
+    <v-flex class="d-inline">
       <div wrap class="d-none d-md-inline">
         <v-btn @click="showModal=true"
              class="filter-button korean mt-2 ml-1 mr-1 lighten-2"
@@ -9,7 +9,7 @@
              rounded
              medium
              depressed
-        >{{label}} {{isFilterOn ? rating : ''}}
+        >{{label}} {{isFilterOn ? ((label === '급여')? ' ( ' + rating + '만원이상 )': ' ( ' + rating + '이상 )' ): ''}}
         </v-btn>
       </div>
       <v-row wrap
@@ -31,7 +31,8 @@
           >
             {{this.label}}
           </v-card-title>
-          <v-card-text>
+
+          <v-card-text  v-if="label === '급여'">
             <v-row wrap style="height: 100px" align="center" justify="center">
               <v-text-field
                       v-model="input"
@@ -40,66 +41,15 @@
                       hide-details
                       label="180"
                       background-color="grey lighten-3"
+                      flat
+                      clearable
               >
               </v-text-field>
               <div class="sub-title-2-large">만원 이상</div>
             </v-row>
           </v-card-text>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn
-                    color="red"
-                    text
-                    @click="cancelModal"
-            >
-              Cancel
-            </v-btn>
 
-            <v-btn
-                    color="primary"
-                    text
-                    @click="confirmModal"
-            >
-              Confirm
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
-    </v-flex>
-
-    <v-flex v-else class="d-inline">
-      <div wrap
-           class="d-none d-md-inline"
-      >
-        <v-btn @click="showModal=true"
-               class="filter-button korean mt-2 ml-1 mr-1 lighten-2"
-               :class="isFilterOn ? 'button-on' : 'grey grey--text text--darken-1'"
-               rounded
-               medium
-        >
-          {{label}} {{isFilterOn ? rating : ''}}
-        </v-btn>
-      </div>
-      <v-row wrap
-             class="d-md-none"
-             justify="center"
-      >
-        <v-flex style="justify-content: space-between">
-          <div class="text-center">
-            <v-btn @click="showModal=true" class="korean mr-1 grey lighten-2 grey--text text--darken-1" small depressed rounded>{{rating}}</v-btn>
-          </div>
-        </v-flex>
-      </v-row>
-
-      <v-dialog v-model="showModal" max-width="500px">
-        <v-card>
-          <v-card-title
-                  class="korean headline grey lighten-2"
-                  primary-title
-          >
-            {{this.label}}
-          </v-card-title>
-          <v-card-text>
+          <v-card-text  v-if="label !== '급여'">
             <v-row wrap style="height: 100px" align="center" justify="center">
               <vue-slider
                       ref="slider"
@@ -115,6 +65,7 @@
               ></vue-slider>
             </v-row>
           </v-card-text>
+
           <v-card-actions>
             <v-spacer></v-spacer>
             <v-btn
@@ -124,7 +75,6 @@
             >
               Cancel
             </v-btn>
-
             <v-btn
                     color="primary"
                     text
