e55a5b6485f46e73d14030b8b4f4a048935d975c
Suniel <sunnynah05@gmail.com>
2019-12-06 22:54:01 +0900

    add signin router

--- SparklingFD/backend/routes/index.js
+++ SparklingFD/backend/routes/index.js
@@ -1,16 +1,49 @@
 var express = require('express');
 var router = express.Router();
+var passport = require("../config/passport");
 
 router.get('/', function (req, res, next) {
   res.sendFile(path.join(__dirname, '../public', 'index.html'))
 });
-// /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
 
-app.get('/signup', function(req, res){
-  res.render('/signup');
-})
+//signin page
+router.get('/signin', function(req, res){
+  var username = req.flash("username")[0];
+  var errors = req.flash("errors")[0] || {};
+  res.render('/signin', {
+    username: username,
+    errors: errors
+  });
+});
+
+//post login
+router.post('/signin', function(req, res, next){
+  var errors = {};
+  var isValid = true;
+  if(!req.body.username) {
+    isValid = false;
+    errors.username = "Username is required";
+  }
+  if(!req.body.password) {
+    isValid = false;
+    errors.password = "Password is required";
+  }
+  if(isValid){
+    next();
+  } else {
+    req.flash("errors", errors);
+    res.redirect('/signin');
+  }
+}, passport.authenticate("local-login",{
+    successRedirect: "/",
+    failureRedirect: "/signin"
+  }
+));
+
+// Logout
+router.get("/logout", function(req, res) {
+  req.logout();
+  res.redirect("/");
+});
 
 module.exports = router;
