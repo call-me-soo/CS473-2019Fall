6b89af1e88bad6b019240cce9658a8b6dcf4584b
Suniel Nah <32695733+Sunnynah@users.noreply.github.com>
2019-12-08 00:45:30 +0900

    Update setup.sh

--- SparklingFD/setup.sh
+++ SparklingFD/setup.sh
@@ -1,12 +1,13 @@
 npm install express-generator -g
-express --view=jade backend
 
 cd frontend
 npm install -g @vue/cli 
 npm install
 npm install axios --save
+npm install --save vuex
 
 cd ../backend
 npm install --save mongoose
 npm install --save body-parser
 npm install
+npm install --save bcrypt-nodejs express-session passport passport-local connect-flash
