c78358eaf22d3abe67b9a2017218e0a2ecb1d9cc
Suniel <sunnynah05@gmail.com>
2019-12-17 16:07:35 +0900

    change review, company  schema

--- SparklingFD/backend/models/company.js
+++ SparklingFD/backend/models/company.js
@@ -2,15 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const reviewSchema = new Schema({
-    id: Number,
-    // company: {type: Schema.Types.Mixed, required: true},
+    // id: Number,
+    company: {type: Schema.Types.Mixed, required: true},
     user: {type: Schema.Types.Mixed, required: true},
     semester: {type: Schema.Types.Mixed, required: true},
-    aggregate: {type: Number, required: true},
-    salary: {type:Number, required: true},
-    salaryPercent: {type:Number, required: true},
-    like: {type: Array, required: true},
-    content: {type:String, required: true}
+    like: {type: Number, required: true},
+    review: Schema.Types.Mixed
 });
 
 const companySchema = new Schema({
