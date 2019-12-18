d8b7d3c29ea536e93cab96ebb53f70ade6f7733b
Suniel <sunnynah05@gmail.com>
2019-12-18 13:42:05 +0900

    implement major recommendation

--- SparklingFD/backend/routes/companies.js
+++ SparklingFD/backend/routes/companies.js
@@ -27,11 +27,17 @@ router.put('/:ID', function(req, res){
       if (err) return res.status(500).send('database failure');
       if (!reviews) return res.status(404).send("reviews not found");
       
+      var sumMajor = {'전산': 0, '기계': 0, '전자': 0, '산디': 0, '생화공': 0, '신소재': 0, '기타': 0};
       var sumSalary = 0;
       var sumStar = [0,0,0,0,0];
       var numReviews = 0;
 
       reviews.map(review => {
+        Object.keys(sumMajor).forEach(e => {
+          if (e == review.user.major){
+            sumMajor[review.user.major]++;
+          }
+        })
         sumSalary += Number(review.review.salary);
         for (var i = 0; i < 5; i++) {
           sumStar[i] += Number(review.review.star[i]);
@@ -39,6 +45,7 @@ router.put('/:ID', function(req, res){
         numReviews ++;
       })
 
+      company.recommend = Object.keys(sumMajor).sort((a, b) => {return Number(sumMajor[b]) - Number(sumMajor[a])}).slice(0,2);
       company.salary = Number((sumSalary / numReviews).toFixed(2));
       for (let i = 0; i < 5; i++) {
         company.star[i] = Number((sumStar[i] / numReviews).toFixed(2))
