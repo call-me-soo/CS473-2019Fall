7082ff519ee1a996ff58413b993ab00278b1f1cf
Suniel Nah <32695733+Sunnynah@users.noreply.github.com>
2019-12-20 03:19:44 +0900

    Update companies.js

--- SparklingFD/backend/routes/companies.js
+++ SparklingFD/backend/routes/companies.js
@@ -10,44 +10,11 @@ router.get('/', function(req, res){
   })
 })
 
-var stars
 router.get('/:ID', function(req, res){
   Company.findOne({ID: req.params.ID}, function(err, company){
     if(err) return res.status(500).json({error: err});
     if(!company) return res.status(404).json({error: 'company not found'});
-    var zero = 0
-    var one = 0
-    var two = 0
-    var three = 0
-    var four = 0
-    console.log("여기부터야야")
-    console.log(company.reviews)
-    if(company.reviews){
-      console.log(company.reviews.length)
-      for (var i = 0; i < company.reviews.length; i++) {
-        console.log(company.reviews[i].review.star[0])
-        console.log(company.reviews[i].review.star[1])
-        console.log(company.reviews[i].review.star[2])
-        console.log(company.reviews[i].review.star[3])
-        console.log(company.reviews[i].review.star[4])
-        zero = zero + company.reviews[i].review.star[0];
-        one = one + company.reviews[i].review.star[1];
-        two = two + company.reviews[i].review.star[2];
-        three = three + company.reviews[i].review.star[3];
-        four = four + company.reviews[i].review.star[4];
-      }
-      company.star[0] = zero / company.reviews.length
-      company.star[1] = one / company.reviews.length
-      company.star[2] = two / company.reviews.length
-      company.star[3] = three / company.reviews.length
-      company.star[4] = four / company.reviews.length
-      stars = company.star
-    }
     res.json(company);
-  })
-  Company.findOneAndUpdate({ID: req.params.ID}, {$set: {star: stars}}, function(err, company){
-    if(err) return res.status(500).json({ error: 'database failure' });
-    if(!company) return res.status(404).json({ error: 'company not found' });
   })
 })
 
