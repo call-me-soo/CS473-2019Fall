6e48b28ae86ac4db137a1f431e3c7890eb7c7af6
Suniel <sunnynah05@gmail.com>
2019-12-21 05:18:41 +0900

    revert bestReviews setting

--- SparklingFD/frontend/src/views/Home.vue
+++ SparklingFD/frontend/src/views/Home.vue
@@ -45,11 +45,7 @@
           </v-col>
         </v-row>
 
-        <ReviewCardSlick
-                v-if="this.bestReviews.length === 10"
-                :bestReviews.sync="bestReviews"
-                class="text-center">
-        </ReviewCardSlick>
+        <ReviewCardSlick :bestReviews.sync="bestReviews" class="text-center"></ReviewCardSlick>
 
         <v-row justify="center" align="center" class="mt-10">
           <v-col cols="9">
